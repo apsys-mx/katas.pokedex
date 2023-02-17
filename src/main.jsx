5384d9da11c82e26325931409bd98aa64a8b865e
Oscar M <oscar.sanchez@apsys.mx>
2023-02-17 13:16:33 -0600

    Add rootview

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './app'
+import RootView from './root-view'
 
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 	<React.StrictMode>
-		<BrowserRouter>
-			<Routes>
-				<Route path='/*' element={<App />} />
-			</Routes>
-		</BrowserRouter>
+		<RootView>
+			<BrowserRouter>
+				<Routes>
+					<Route path='/*' element={<App />} />
+				</Routes>
+			</BrowserRouter>
+		</RootView>
 	</React.StrictMode>
 )
---
+++ src/root-view.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+/**
+ * Root view component
+ * @param {*} props
+ * @returns
+ */
+const RootView = (props) => {
+	return props.children
+}
+export default RootView
